6cee46ca3d1e892c619488f0d5d34afd5dc9c093
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2023-04-30 20:24:10 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,14 +1,35 @@
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from dash import Dash, dcc, Output, Input
+import dash
+from dash import Dash, dcc, Output, Input, State
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
 
+
+### Build the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR], title="IVCurves.com")
+server = app.server
+
+
+###Module things
 df_names = pd.read_excel('modules.xlsx')
 df_voltage = pd.read_excel('modules.xlsx', sheet_name='Voltage')
 df_current = pd.read_excel('modules.xlsx', sheet_name='Current')
+dropdown_mods = dcc.Dropdown(options=df_names.iloc[:,0],
+                        clearable=False)
 
+
+###Parameter things
+dropdown_parameters = dcc.Dropdown(options=['Pmp', 'Power Curve'],
+                        clearable=True)
+
+
+###Analyze things
+dropdown_analyze = dcc.Dropdown(options=['Degrade', 'Scale'],
+                        clearable=True)
 
 df_IV=pd.DataFrame({            #initializes df as first mod
     'Voltage': df_voltage.iloc[:,0],
@@ -16,21 +37,113 @@ df_IV=pd.DataFrame({            #initializes df as first mod
     })
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
-server = app.server
-mytitle = dcc.Markdown(children="IV Curve Database & Calculator")
-mygraph = dcc.Graph(figure={})
-dropdown_mods = dcc.Dropdown(options=df_names.iloc[:,0],
-                        clearable=False)
-dropdown_options = dcc.Dropdown(options=['Pmp', 'Power Curve'],
-                        clearable=True)
+app.layout = dbc.Container(
+    [
+        dbc.Row([
+            dbc.Col([
+                dcc.Markdown("""
+                # IV Curve Database & Analysis
+                """)
+            ], width=True),
+        ], align="end"),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col([
+                dbc.Button(
+                    "Select Module",
+                    id="module_button"
+                ),
+                dbc.Collapse(
+                    dbc.Card(
+                        dbc.CardBody(
+                            dropdown_mods,
+                        )
+                    ),
+                    id="module_collapse",
+                    is_open=False
+                ),
+                html.Hr(),
+                dbc.Button(
+                    "Add Parameters",
+                    id="parameter_button"
+                ),
+                dbc.Collapse(
+                    dbc.Card(
+                        dbc.CardBody(
+                            dropdown_parameters,
+                        )
+                    ),
+                    id="parameter_collapse",
+                    is_open=False
+                ),
+                html.Hr(),
+                dbc.Button(
+                    "Analyze",
+                    id="analyze_button"
+                ),
+                dbc.Collapse(
+                    dbc.Card(
+                        dbc.CardBody(
+                            dropdown_analyze,
+                        )
+                    ),
+                    id="analyze_collapse",
+                    is_open=False
+                ),
+                html.Hr(),
+            ], width=3),
+            dbc.Col([
+                dcc.Graph(id='display', style={'height': '90vh'}),
+            ], width=9, align="start")
+        ]),
+    ],
+    fluid=True
+)
 
-app.layout = dbc.Container([mytitle, mygraph, dropdown_mods, dropdown_options])
+### Callback to make parameters menu expand
+@app.callback(
+    Output("parameter_collapse", "is_open"),
+    [Input("parameter_button", "n_clicks")],
+    [State("parameter_collapse", "is_open")]
+)
+def toggle_shape_collapse(n_clicks, is_open):
+    if n_clicks:
+        return not is_open
+    return is_open
+
+
+### Callback to make module select menu expand
+@app.callback(
+    Output("module_collapse", "is_open"),
+    [Input("module_button", "n_clicks")],
+    [State("module_collapse", "is_open")]
+)
+def toggle_shape_collapse(n_clicks, is_open):
+    if n_clicks:
+        return not is_open
+    return is_open
+
+
+### Callback to make analyze menu expand
+@app.callback(
+    Output("analyze_collapse", "is_open"),
+    [Input("analyze_button", "n_clicks")],
+    [State("analyze_collapse", "is_open")]
+)
+def toggle_shape_collapse(n_clicks, is_open):
+    if n_clicks:
+        return not is_open
+    return is_open
+
+
+###############
+#mygraph = dcc.Graph(figure={})
 
 @app.callback(
-    Output(mygraph, component_property='figure'),
+    Output("display", component_property='figure'),
     Input(dropdown_mods, 'value'),
-    Input(dropdown_options, 'value')
+    Input(dropdown_parameters, 'value')
+    #Input(dropdown_analyze, 'value')
 )
 
 def update_graph(selected_mod, selected_option):
@@ -50,21 +163,30 @@ def update_graph(selected_mod, selected_option):
         )
     fig.update_xaxes(title_text="Voltage")
     fig.update_yaxes(title_text="Current", secondary_y=False)
+    df['Power'] = df['Voltage'] * df['Current']
+    Pmp = df.loc[df['Power']==df['Power'].max()]
+    
     if selected_option == "Power Curve":
-        df_pow = pd.DataFrame({
-            'Voltage': df['Voltage'],
-            'Power': df['Voltage'] * df['Current']
-            })       
         fig.add_trace(
             go.Scatter(
-                x=df_pow.loc[:,"Voltage"],
-                y=df_pow.loc[:,"Power"],
+                x=df.loc[:,"Voltage"],
+                y=df.loc[:,"Power"],
                 mode="lines",
                 line_color="goldenrod",
                 showlegend=False),
             secondary_y=True
             )
         fig.update_yaxes(title_text="Power", secondary_y=True)
+    
+    if selected_option == "Pmp":
+        fig.add_trace(
+            go.Scatter(
+                x=Pmp.loc[:,"Voltage"],
+                y=Pmp.loc[:,"Current"],
+                line_color="red",
+                showlegend=False),
+            secondary_y=False
+            )
     return fig
 
 if __name__=='__main__':
